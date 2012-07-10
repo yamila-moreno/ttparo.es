4c1d3c9efb682c8039b86ad3141e7ed7c3f1cc76
Yamila Moreno <yamila.moreno@kaleidos.net>
2012-07-10 09:33:55 +0200

    Tests

--- src/tasaparo/core/tests.py
+++ src/tasaparo/core/tests.py
@@ -6,11 +6,23 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.utils import simplejson
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class MicroDataTest(TestCase):
+
+    fixtures = ['initial_data']
+
+    def test_show_microdata(self):
+        url = reverse('api:show-microdata')
+        post_data = {}
+        response = self.client.post(url, post_data)
+        self.assertEqual(response.status_code, 200)
+
+        json_parsed = simplejson.loads(response.content)
+        self.assertTrue(json_parsed['success'])
+
+        self.assertIsInstance(json_parsed['tasaparo'], int)
+
+

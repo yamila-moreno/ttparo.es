3f03e3907ed6c2472e79563e62991b300c9ccba8
Juanfran <juanfran.alcantara@kaleidos.net>
2012-07-12 14:52:08 +0200

    graphic page

--- src/tasaparo/web/static/js/general.js
+++ src/tasaparo/web/static/js/general.js
@@ -6,8 +6,16 @@ var TtpRouter = Backbone.Router.extend({
         "profile/" : "profile",
     },
 
-    home: function(){
-        console.log("home");
+    home: function(){            
+        $("#inner-content").html("<div id='home-view'></div>");
+        var homeview = new HomeView();
+        homeview.render();
+
+        var inforesult = new InfoResult();
+        inforesult.render();
+
+        var lastprofiles = new LastProfiles();
+        lastprofiles.render();
     },
 
     map: function(){
@@ -19,32 +27,135 @@ var TtpRouter = Backbone.Router.extend({
     },
 
     profile: function(){
-        console.log("profile");
+        var profileview = new ProfileView();
+        profileview.render();
     }
 });
 
-$(document).ready(function(){    
-    $('.default').dropkick();
-    
-    var appTtpRouter = new TtpRouter();
-    Backbone.history.start({pushState: true});
+var RecalculateView = Backbone.View.extend({
+    template: _.template($("#recalculate").html()),
+    render: function() {
+        $("#left").append(this.template());
+        return this;
+    }
+});
 
-    $(".result").tooltip({ 
-        'position': 'top center',
-        'margin_bottom': 10,
-        'html': function(self){
-            return "<p>Rellena el formulario para obtener tu tasa de paro</p><div class='arrow'></div></div>";
-         }                    
-    });
+var InfoResult = Backbone.View.extend({
+    template: _.template($("#info-result").html()),
+    render: function() {
+        $("#inner-content").append(this.template());
+        $('.default').dropkick();
 
-    $("#calculate").on("submit", function(e){
+        return this;
+    }
+});
+
+var ProfileView = Backbone.View.extend({
+    template: _.template($("#result-profile").html()),
+    el: '#profile-view',
+    values: {
+      serie1 : [19, 35, 9, 18, 19, 35, 9, 18],
+      serie2 : [17, 57, 4, 85, 17, 57, 4, 85],
+      serie3 : [27, 67, 7, 35, 27, 67, 7, 35],
+    },
+    initchart: function(){
+        $("#chart").chart({
+         type : "line",
+         labels : ["2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012"],
+         values : this.values,
+         margins : [10, 15, 20, 50],
+        series: {
+              serie1 : {
+               color : "red"
+              },
+              serie2 : {
+               color : "blue"
+              }
+        },
+         defaultAxis : {
+          labels : true
+         },
+         features : {
+          grid : {
+           draw : true,
+           forceBorder : true
+          }
+         }
+        });
+    },
+    render: function() {
+        $("#inner-content").html("<div id='profile-view'></div>");
+        $("#profile-view").html(this.template());
+
+        this.initchart();
+
+        var recalcualteview = new RecalculateView();
+        recalcualteview.render();
+
+        $("#calculate").bind("submit", $.proxy( this.submit, this ));
+
+        var inforesult = new InfoResult();
+        inforesult.render();
+
+        return this;
+    },
+    submit: function(e){
+        e.preventDefault();
+        var self = this;
+
+        $.ajax({
+          data: $(this).serialize(),    
+          url: $(this).attr('action'),
+          success: function(data) {
+            //delete
+             data =  {
+              serie1 : [19, 35, 29, 18, 19, 35, 9, 18],
+              serie2 : [37, 57, 4, 15, 17, 17, 34, 85],
+              serie3 : [7, 67, 7, 35, 57, 67, 7, 35],
+            };
+
+            self.values = data;
+
+            $("#chart").fadeOut(function(){
+                $(this).remove();
+                $("#right").html("<div id='chart'></div>");
+                self.initchart();
+            });
+
+          }
+        });
+    }
+});
+
+var LastProfiles = Backbone.View.extend({
+    template: _.template($("#last-profiles").html()),
+    render: function() {
+        $("#inner-content").append(this.template());
+        return this;
+    }
+});
+
+var HomeView = Backbone.View.extend({
+    template1: _.template($("#result-empty-template").html()),      
+    template2: _.template($("#result-template").html()), 
+    data: false,    
+    el: '#home-view',
+    render: function() {
+        $(this.el).html(this.template1());
+
+        $('.default').dropkick();
+        $("#calculate").bind("submit", this.submit);
+
+        return this;
+    },
+    submit: function(e){
         e.preventDefault();
 
         $.ajax({
           data: $(this).serialize(),    
           url: $(this).attr('action'),
           success: function(data) {
-            data = {'result': 17, 'level': 'r1', 'leveltxt': 'nivel alto'};
+            data = {'result': 17, 'level': 'r2', 'leveltxt': 'nivel alto'};
 
             $("#resulparo").fadeOut(function(){
                 var template = _.template($("#result-template").html());
@@ -54,6 +165,19 @@ $(document).ready(function(){
 
           }
         });
+    }
+});
+
+$(document).ready(function(){    
+    var appTtpRouter = new TtpRouter();
+    Backbone.history.start({pushState: true});
+
+    $(".result").tooltip({ 
+        'position': 'top center',
+        'margin_bottom': 10,
+        'html': function(self){
+            return "<p>Rellena el formulario para obtener tu tasa de paro</p><div class='arrow'></div></div>";
+         }                    
     });
 });
 

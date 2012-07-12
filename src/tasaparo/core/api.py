80f569c0e941ade13a6b8e31279a26ebd81a5332
Juanfran <juanfran.alcantara@kaleidos.net>
2012-07-12 18:55:37 +0200

    init map

--- src/tasaparo/core/api.py
+++ src/tasaparo/core/api.py
@@ -10,7 +10,7 @@ from django.shortcuts import get_object_or_404
 from django.forms.models import model_to_dict
 
 from tasaparo.core import models as core
-from tasaparo.core.forms import FilterForm
+#from tasaparo.core.forms import FilterForm
 
 ######################################
 #  TODO: form to validate input data

373bbc81ef5309110d204204eb72aae8573ad48f
Yamila Moreno <yamila.moreno@kaleidos.net>
2012-07-13 11:06:00 +0200

    provincias ordenadas

--- src/tasaparo/core/forms.py
+++ src/tasaparo/core/forms.py
@@ -9,7 +9,7 @@ class FilterForm(forms.Form):
     AGE_CHOICES = [('',u'Cualquier edad')] + list(models.Age.objects.all().values_list('id','name'))
     CYCLE_CHOICES = models.Microdata.objects.all().order_by('cycle').distinct('cycle').values_list('cycle', 'cycle')
     EDUCATION_CHOICES = [('',u'Formación indiferente')] + list(models.Education.objects.all().values_list('id','name'))
-    PROVINCE_CHOICES = [('',u'Todas las provincias')] + list(models.Province.objects.all().values_list('id','name'))
+    PROVINCE_CHOICES = [('',u'Todas las provincias')] + list(models.Province.objects.all().order_by('name').values_list('id','name'))
     SEX_CHOICES = [('',u'Cualquiera')] + list(models.Sex.objects.all().values_list('id','name'))
 
     age = forms.TypedChoiceField(required=False, choices=AGE_CHOICES, widget=forms.Select(attrs={'class':'default'}))

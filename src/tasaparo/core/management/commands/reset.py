be0b702097261dcaa586a4eb23bf2a59cb12b198
Yamila Moreno <yamila.moreno@kaleidos.net>
2012-07-23 21:08:55 +0200

    user tasaparo in commands/reset.py

--- src/tasaparo/core/management/commands/reset.py
+++ src/tasaparo/core/management/commands/reset.py
@@ -47,7 +47,7 @@ class Command(BaseCommand):
 
 
     def load_microdata_sql(self, sql):
-        subprocess.Popen(['psql', '-f', sql, 'tasaparo']).communicate()
+        subprocess.Popen(['psql', '-f', sql, '-U', 'tasaparo', 'tasaparo']).communicate()
 
     @transaction.commit_on_success
     def load_micdrodata_csv(self, csv):

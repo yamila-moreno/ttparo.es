7aa3d7d6b07f68fa66cbf69910a2d4e64fd3d67f
Xavier Julián <xavier.julian@kaleidos.net>
2013-01-23 12:59:57 +0100

    Widget fixes

--- src/tasaparo/web/templates/widget.js
+++ src/tasaparo/web/templates/widget.js
@@ -1,29 +1,41 @@
 (function() {
     var kaleidosDiv = document.getElementById("kaleidos-tasaparo");
 
-    var moreInfoLink = document.createElement('a');
-    moreInfoLink.href = "{{ url }}{{ absolute_url }}";
-    moreInfoLink.target = "_new";
-    kaleidosDiv.appendChild(moreInfoLink);
+    var logoLink = document.createElement('a');
+    logoLink.setAttribute("href", "{{ url }}{{ absolute_url }}");
+    logoLink.setAttribute("class", "logo-link");
+    logoLink.setAttribute("target", "_new");
+    kaleidosDiv.appendChild(logoLink);
 
     var logo = document.createElement('img');
     logo.src = "{{ url }}/static/images/logo_widget.png";
-    moreInfoLink.appendChild(logo);
+    logoLink.appendChild(logo);
 
     var rateValue = document.createElement('div');
     rateValue.className = "result r" + {{ level }};
     rateValue.textContent = {{ rate }}+"%";
-    moreInfoLink.appendChild(rateValue);
+    rateValue.setAttribute("href", "{{ url }}{{ absolute_url }}");
+    kaleidosDiv.appendChild(rateValue);
 
     var moreInfoText = document.createElement('div');
     moreInfoText.className = "txt";
-    moreInfoText.innerHTML = "+ INFO";
-    moreInfoLink.appendChild(moreInfoText);
+    kaleidosDiv.appendChild(moreInfoText);
+    
+    var moreInfoLink = document.createElement('a');
+    moreInfoLink.setAttribute("href", "{{ url }}{{ absolute_url }}");
+    moreInfoLink.setAttribute("target", "_new");
+    moreInfoLink.innerHTML = "+ Info";
+    moreInfoText.appendChild(moreInfoLink);
 
     var stylesheet = document.createElement('link');
     stylesheet.href = "{{ url }}/static/css/widget.css";
     stylesheet.rel = "stylesheet";
     stylesheet.type = "text/css";
     kaleidosDiv.appendChild(stylesheet);
+    
+    $('#kaleidos-tasaparo').click( function() {
+        var link = "{{ url }}{{ absolute_url }}";
+        window.open(link);
+    });
 
 }).call(this);

0aa807a8a7bc9dd8ef2fb5133850e0f597306108
Yamila Moreno <yamila.ms@gmail.com>
2013-01-25 16:17:00 +0100

    Fixed copy

--- src/tasaparo/core/forms.py
+++ src/tasaparo/core/forms.py
@@ -8,7 +8,7 @@ class FilterForm(forms.Form):
 
     AGE_CHOICES = [('',u'Todas las edades')] + list(models.Age.objects.all().values_list('id','name'))
     CYCLE_CHOICES = models.Microdata.objects.all().order_by('cycle').distinct('cycle').values_list('cycle', 'cycle')
-    EDUCATION_CHOICES = [('',u'Formación indiferente')] + list(models.Education.objects.all().values_list('id','name'))
+    EDUCATION_CHOICES = [('',u'Cualquier nivel de estudios')] + list(models.Education.objects.all().values_list('id','name'))
     PROVINCE_CHOICES = [('',u'Todas las provincias')] + list(models.Province.objects.all().order_by('name').values_list('id','name'))
     SEX_CHOICES = [('',u'Ambos')] + list(models.Sex.objects.all().values_list('id','name'))
 
